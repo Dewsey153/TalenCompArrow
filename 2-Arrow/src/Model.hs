c0302ca0bc55d2e0ecd2c145b8d98930f16e4fb0
Rens de Wit <r.dewit1@students.uu.nl>
2024-12-05 16:36:09 +0100

    Make data types for the whole program, to which the strings will get parsed.

--- 2-Arrow/src/Model.hs
+++ 2-Arrow/src/Model.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 module Model where
 
 -- Exercise 1
@@ -28,4 +30,46 @@ data Token =
      deriving Show
 
 -- Exercise 2
-data Program = Program deriving Show
+data Program = 
+  Program [Rule]
+    deriving Show
+data Rule = Rule IIdent Cmds
+    deriving Show
+
+data Cmds = CmdsEmpty | Cmd Cmd [Cmd]
+    deriving Show
+data Cmd = 
+    CGo 
+  | CTake 
+  | CMark 
+  | CNothing
+  | CTurn Dir
+  | CCaseOfEnd Dir Alts
+  | CIdent IIdent
+    deriving Show
+
+data Dir =
+     Left
+    |Right
+    |Front
+  deriving Show
+
+data Alts =
+     AltsEmpty
+     | Alts Alt [Alt]
+  deriving Show
+
+data Alt = Alt Pat Cmds
+  deriving Show
+
+data IIdent = IIdent String
+  deriving Show
+
+data Pat = 
+     PEmpty
+    |PLambda
+    |PDebris
+    |PAsteroid
+    |PBoundary
+    |PUnderscore
+  deriving Show
