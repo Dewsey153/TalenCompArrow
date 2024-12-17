7d57f18a37cbd6cbd7ffd1659794063643b18e86
Rens de Wit <r.dewit1@students.uu.nl>
2024-12-17 19:05:25 +0100

    Try to make function for interactive

--- 2-Arrow/app/Main.hs
+++ 2-Arrow/app/Main.hs
@@ -10,8 +10,25 @@ import Data.Map
 import qualified Data.HashSet as HashSet
 
 -- Exercise 11
+
+-- The interactive driver. After each step, print the board and
+-- ask the user for confimation before continuing.
 interactive :: Environment -> ArrowState -> IO ()
 interactive = undefined
+
+-- 
+-- interactive :: Step -> Environment -> ArrowState -> IO ()
+-- interactive oldStep env oldState =
+--    let
+--       newStep = step env oldState
+--    in
+--       print $ message newStep
+--       getChar
+--       return $ case newStep of
+--         Ok newState
+--           -> interactive newStep env newState
+--         _ 
+--           -> () 
 
 batch :: Environment -> ArrowState -> (Space, Pos, Heading)
 batch = undefined
@@ -35,3 +52,25 @@ main = do
    putStrLn ""
    print arr
 
+-- Provided the path to the file containing the space, parse
+-- the Space and return it.
+readSpace :: FilePath -> IO Space
+
+-- Given the file path to the program, parse the program.
+readProgram :: FilePath -> IO Program
+
+-- Ask the user to provide a starting position
+getStartPosition :: IO Position
+
+-- Ask the user to provide a starting heading for Arrow
+getInitialHeading :: IO Heading
+
+message :: Step -> String
+message (Done space pos heading) 
+   = printSpace space ++ "\r\nThe program has terminated without any errors."
+      
+message (Ok (ArrowState space pos heading stack)) 
+   = printSpace space ++ "\r\nPress any key to continue."
+      
+message (Fail errorMessage) 
+   = errorMessage
